e081b90223289836b33dc349ec91335902c2e0f5
dev2 <dev2@mail.com>
2023-02-16 13:19:23 +0530

    dev1 modf hii.java

--- hii.java
+++ hii.java
@@ -1,7 +1,7 @@
-class hello
+class hello  #from dev1
 {
-	public static void main ()
+	public static void main ()  #from dev1
 	{
-		system.out.println("hello")
+		system.out.println("hello from dev1")
 	}
-}
+ }
